165e8cc584acb797fd8fc3062f46acbda1dfbfd1
Aleksa Savić <10512545+aleksamagicka@users.noreply.github.com>
2023-03-06 13:30:18 +0100

    Initial commit

---
+++ Data/Lek.cs
@@ -0,0 +1,11 @@
+﻿namespace diplomskirad.Data
+{
+    public class Lek
+    {
+        public int Id { get; set; }
+        public string VrstaResenja { get; set; }
+        public string NazivLeka { get; set; }
+        public string INN { get; set; }
+
+    }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace diplomskirad.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
