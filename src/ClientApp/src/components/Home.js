a0de952772f47b285a9bad4791deb7a7436b3afb
Aleksa Savić <10512545+aleksamagicka@users.noreply.github.com>
2023-03-10 11:40:34 +0100

    Added an overview of features to home page

--- src/ClientApp/src/components/Home.js
+++ src/ClientApp/src/components/Home.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
+﻿import React, { Component } from 'react';
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
 
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1>Implementacija registra lekova za upotrebu u humanoj medicini</h1>
+                <p>Dobro došli! Ova web aplikacija predstavlja projekat na temu mog diplomskog rada. Korišćene su sledeće tehnologije:</p>
+                <ul>
+                    <li><a href='https://get.asp.net/'>ASP.NET Core</a> i <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> za <i>backend</i> deo,</li>
+                    <li><a href='https://www.postgresql.org/'>PostgreSQL</a> relaciona baza za čuvanje podataka,</li>
+                    <li><a href='https://facebook.github.io/react/'>React</a> za <i>frontend</i> deo, koji upravo vidite i </li>
+                    <li><a href='http://getbootstrap.com/'>Bootstrap</a> za ujedačen stil i dizajn sajta</li>
+                </ul>
+                <p>Pritom, od pomena je i <a href='https://www.material-react-table.com/'>Material React Table</a> biblioteka, čije su mnogobrojne funkcionalnosti iskorišćene za <a href='fetch-data'>prikaz samih podataka</a>.</p>
+                <p>Moguće je <a href='authentication/login'>ulogovati se</a> kao administrator, sa kredencijalima <code>admin@example.com</code>/<code>Admin123@</code>. Odatle je moguće promeniti profilne podatke, kao i učitati nov registar lekova. </p>
+                <br></br>
+                <p style={{ textAlign: 'right' }}>Autor: <a href='https://github.com/aleksamagicka'>Aleksa Savić</a> (2019/0595)</p>
+            </div>
+        );
+    }
 }
